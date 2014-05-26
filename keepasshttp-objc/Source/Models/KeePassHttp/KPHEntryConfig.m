3b843d75c59b90e6d5b25593db5150d8a5994b3f
XBigTK13X <xbigtk13x@gmail.com>
2014-05-26 10:26:31 -0500

    Flesh out the EntryConfig stub

--- keepasshttp-objc/Source/Models/KeePassHttp/KPHEntryConfig.m
+++ keepasshttp-objc/Source/Models/KeePassHttp/KPHEntryConfig.m
@@ -7,6 +7,8 @@
 //
 
 #import "KPHEntryConfig.h"
+#import "KPHSystemConvert.h"
+#import "NSDictionary_JSONExtensions.h"
 
 @implementation KPHEntryConfig
 - (id) initWithJson:(NSString*)json
@@ -14,12 +16,54 @@
     self = [super init];
     if(self)
     {
-        //TODO Convert json to KPHEC format
+        NSError *error = NULL;
+        NSDictionary *jsonDict = [NSDictionary dictionaryWithJSONString:json error:&error];
+        if(jsonDict[@"Realm"] != nil){
+            self.Realm = jsonDict[@"Realm"];
+        }
+        self.Allow = [NSMutableSet new];
+        if(jsonDict[@"Allow"] != nil){
+            for(NSString* allowed in jsonDict[@"Allow"]){
+                [self.Allow addObject:allowed];
+            }
+        }
+        self.Deny = [NSMutableSet new];
+        if(jsonDict[@"Deny"] != nil){
+            for(NSString* denied in jsonDict[@"Deny"]){
+                [self.Deny addObject:denied];
+            }
+        }
     }
     return self;
 }
 - (NSString*) toJson
 {
-    return nil;
+    NSMutableString* json = [NSMutableString new];
+    [json appendString:@"{"];
+    if(self.Realm != nil){
+        [json appendString: [KPHSystemConvert toJSON:@"Realm" value:self.Realm]];
+        [json appendString:@","];
+    }
+    if(self.Allow != nil){        
+        [json appendString:@"\"Allow\":["];
+        for(NSString* allowed in self.Allow){
+            [json appendString:[NSString stringWithFormat:@"\"%@\",",allowed]];
+        }
+        [json deleteCharactersInRange:NSMakeRange([json length]-1, 1)];
+        [json appendString:@"]"];
+        [json appendString:@","];
+    }
+    if(self.Deny != nil){
+        [json appendString:@"\"Deny\":["];
+        for(NSString* denied in self.Deny){
+            [json appendString:[NSString stringWithFormat:@"\"%@\",",denied]];
+        }
+        [json deleteCharactersInRange:NSMakeRange([json length]-1, 1)];
+        [json appendString:@"]"];
+        [json appendString:@","];
+    }
+    [json deleteCharactersInRange:NSMakeRange([json length]-1, 1)];
+    [json appendString:@"}"];
+    return json;
 }
 @end
